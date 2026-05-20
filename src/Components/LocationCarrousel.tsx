be0d6b7d149bc5f221d57434484acc4bc23af915
RuralGiovane <giovaneamato@gmail.com>
2026-05-20 19:17:48 -0300

    Feat: Creating Carrousel with location unit

--- src/Components/LocationCarrousel.tsx
+++ src/Components/LocationCarrousel.tsx
@@ -1,9 +1,37 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import { locations } from "../Data/LocationData";
+import { ChevronRight } from "lucide-react-native";
 
 export function LocationCarrousel() {
     return (
-        <View className="flex-1 items-center justify-center h-screen bg-white">
-            <Text>Localização</Text>
+        <View className="mt-4">
+            <View className="flex-row items-center justify-start space-x-4 px-4">
+                {locations.map((item) => (
+                    <TouchableOpacity
+                        key={item.identifier}
+                        activeOpacity={0.85}
+                        onPress={() => Linking.openURL(item.linkMaps)}
+                    >
+                        <View className="w-44 h-56 bg-white rounded-2xl shadow-md overflow-hidden">
+                            <Image source={item.img} className="w-full h-28" resizeMode="cover" />
+                            <View className="p-3 flex-1 justify-between">
+                                <View>
+                                    <Text className="text-neutral-900 text-sm font-semibold mb-1" numberOfLines={1}>
+                                        {item.name}
+                                    </Text>
+                                    <Text className="text-neutral-600 text-xs" numberOfLines={2}>
+                                        {item.location}
+                                    </Text>
+                                </View>
+                                <View className="mt-3 items-center justify-center bg-blue/10 rounded-full py-1 flex-row space-x-1">
+                                    <Text className="text-blue text-xs font-semibold">Ver Unidade</Text>
+                                    <ChevronRight size={16} color="#1F6AE1" />
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                ))}
+            </View>
         </View>
     );
 }
