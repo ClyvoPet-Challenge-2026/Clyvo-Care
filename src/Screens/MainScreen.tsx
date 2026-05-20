57bc15fe26cf4ace189396b2f20e0a9b14d9a803
RuralGiovane <giovaneamato@gmail.com>
2026-05-20 17:16:29 -0300

    Chore: Removing logout button

--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -1,25 +1,22 @@
-import {View, Text, TouchableOpacity} from "react-native"
-import { useAuth } from "../Context/AuthContext";
+import {View, Text} from "react-native"
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 export function MainScreen() {
-  const { logout } = useAuth();
-
   return (
-    <View className="flex-1 items-center justify-center h-screen bg-white">
-      <View className="items-center justify-center">
-        <Text className="text-neutral-900 text-3xl font-bold mb-4">
-          Bem-vindo!
-        </Text>
-        <Text className="text-neutral-600 text-base text-center mb-8">
-          Você foi redirecionado com sucesso
-        </Text>
-        <TouchableOpacity 
-          onPress={logout}
-          className="bg-red-500 rounded-lg p-3 px-8"
-        >
-          <Text className="text-white text-lg font-semibold">Fazer Logout</Text>
-        </TouchableOpacity>
+    <View className="flex-1 bg-mainBackground">
+      <Header />
+      <View className="flex-1 items-center justify-center h-screen">
+        <View className="items-center justify-center">
+          <Text className="text-neutral-900 text-3xl font-bold mb-4">
+            Bem-vindo!
+          </Text>
+          <Text className="text-neutral-600 text-base text-center mb-8">
+            Você foi redirecionado com sucesso
+          </Text>
+        </View>
       </View>
+      <Footer />
     </View>
   );
 }
