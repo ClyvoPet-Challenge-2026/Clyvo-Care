93e554e91bdf0a50b4ecbedd0338f18216a31be2
RuralGiovane <giovaneamato@gmail.com>
2026-05-20 17:12:15 -0300

    Style: Changing text position and removing border

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 
 export default function Footer() {
     return (
-        <View className="border-t-2 border-gray-700 flex-row fixed bottom-0 w-screen bg-gray-100">
+        <View className="flex-row fixed bottom-0 w-screen justify-center bg-gray-100">
             <Text className="text-center text-gray-500 p-4">© 2026 Clyvo Care. All rights reserved.</Text>
         </View>
     )
