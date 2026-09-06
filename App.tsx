8b7b5b32d215c386427954cd332051914f2a9327
RuralGiovane <giovaneamato@gmail.com>
2026-09-06 00:50:13 -0300

    fix: resolve metro fast refresh crash and update nativewind dependencies

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import './global.css';
 import AppNavigator from "./src/Navigation/AppNavigator"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from "./src/Context/AuthContext";
+
 
 export default function App() {
   return (
